15f6e460b778a6c41866bc859d1e2a28f4d0cd09
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-10-23 12:26:37 +0200

    created ItemFactory class

---
+++ Java/src/main/java/com/gildedrose/ItemFactory.java
@@ -0,0 +1,17 @@
+package com.gildedrose;
+
+public class ItemFactory {
+    
+    public static Item getItem (String name, int sellIn, int quality) {
+        if (name.equals("Aged Brie")) {
+            return new AgedBrie(name, sellIn, quality);
+        }
+        if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return new BackstagePass(name, sellIn, quality);
+        }
+        if (name.equals("Sulfuras, Hand of Ragnaros")) {
+            return new Sulfuras(name, sellIn, quality);
+        }
+        return new ItemForSale(name, sellIn, quality);
+    }
+}
