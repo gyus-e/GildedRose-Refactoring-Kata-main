5e2e5b0464a2ab0cae831e99c75fd295c4422f65
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2024-10-28 19:13:55 +0100

    UpdateStrategy

--- src/model/GildedRose.java
+++ src/model/GildedRose.java
@@ -2,22 +2,14 @@ package model;
 
 public class GildedRose {
     Item[] items;
-    ItemForSale[] itemsForSale;
 
     public GildedRose(Item[] items) {
     	this.items=items;
-    	this.itemsForSale=new ItemForSale[items.length];
-    	
-        for (int i = 0; i < items.length; i++) {
-            itemsForSale[i]=(ItemFactory.getItemForSale(items[i].name, items[i].sellIn, items[i].quality));
-        }
     }
 
     public void updateQuality() {
-        for (int i = 0; i < itemsForSale.length; i++) {
-            itemsForSale[i].updateQuality();
-            items[i].sellIn=itemsForSale[i].sellIn;
-            items[i].quality=itemsForSale[i].quality;
+        for (Item item : items) {
+            UpdateStrategy.getUpdateStrategy(item);
         }
     }
 }
---
+++ src/model/UpdateStrategy.java
@@ -0,0 +1,10 @@
+package model;
+
+public class UpdateStrategy {
+    static void getUpdateStrategy (Item item) {
+        ItemForSale workingItem = ItemFactory.getItemForSale(item.name, item.sellIn, item.quality);
+        workingItem.updateQuality();
+        item.quality=workingItem.quality;
+        item.sellIn=workingItem.sellIn;
+    }
+}
